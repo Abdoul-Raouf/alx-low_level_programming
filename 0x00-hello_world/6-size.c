657b3d1fdb5c4ec5fa4c2f27a57a3b7f9d999de7
Abdoul-Raouf <mukailaalhshittuabdoul@gmail.com>
2023-03-18 17:45:38 -0700

    adding

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(voide){
+	int x;
+	char a;
+	long int moy;
+	long long int prix;
+	float electron;
+	printf("Size of a char: %lu", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu", (unsigned long)sizeof(x));
+	printf("Size of a long int: %lu", (unsigned long)sizeof(moy));
+	printf("Size of a long long int: %lu", (unsigned long)sizeof(prix));
+	printf("Size of a float: %lu", (unsigned long)sizeof(electron));
+}
