60c11a46bd1152012052f5e8eab616988b3ace1d
Abdoul-Raouf <mukailaalhshittuabdoul@gmail.com>
2023-03-18 17:54:07 -0700

    adding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-int main(voide){
+
+int main(void)
+{
 	int x;
 	char a;
 	long int moy;
 	long long int prix;
 	float electron;
-	printf("Size of a char: %lu", (unsigned long)sizeof(a));
-	printf("Size of a int: %lu", (unsigned long)sizeof(x));
-	printf("Size of a long int: %lu", (unsigned long)sizeof(moy));
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(prix));
-	printf("Size of a float: %lu", (unsigned long)sizeof(electron));
+
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(s)", (unsigned long)sizeof(x));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(moy));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(prix));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(electron));
 	return (0);
 }
